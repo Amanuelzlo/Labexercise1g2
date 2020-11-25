129023a101bc52497d83af18c04454fac525d366
Amanuelzlo <51112997+Amanuelzlo@users.noreply.github.com>
2020-11-25 12:26:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
   <head>
-    <title>Display Hello World! </title>
+    <title>Display Hello World!</title>
   </head>
   <body>
     <h1> hello world</h1>
+    <h1> Actually, Normally and horrible Mic</h1>
   </body>
 </html>
